5f45939b0f25d72c03cefa4a196dd42500f9a0a9
Tom Barnes <ruralmyth@gmail.com>
2017-03-13 21:44:35 +0000

    can add multiple entries per day on multiple lines

--- dear-diary.sh
+++ dear-diary.sh
@@ -6,13 +6,18 @@ DIARY_FILE="diary.txt"
 
 function main {
   echo "Dear diary... Enter 'end' if you have no more entries for today."
-  echo "Who did you pair with?"
-  printf "> "
-  read PAIR_PARTNER
-  echo "What did you do?"
-  printf "> "
-  read DETAILS
-  echo "$(date '+%A %d %B %Y'), Paired with $(echo $PAIR_PARTNER | awk '{print toupper($0)}'): $DETAILS" >> $DIARY_FILE
+  while true; do
+    echo "Who did you pair with?"
+    printf "> " && read PAIR_PARTNER
+    
+    if [[ $PAIR_PARTNER = "end" ]]; then
+      exit 0
+    fi
+    
+    echo "What did you do?"
+    printf "> " && read DETAILS
+    echo "$(date '+%A %d %B %Y'), Paired with $(echo $PAIR_PARTNER | awk '{print toupper($0)}'): $DETAILS" >> $DIARY_FILE
+  done
 }
 
 main

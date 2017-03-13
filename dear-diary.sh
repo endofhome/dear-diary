e4aff28de29819e11a400fe455f56f7179ffbc50
Tom Barnes <ruralmyth@gmail.com>
2017-03-13 21:32:43 +0000

    initial commit, write out single pairing session to text file

---
+++ dear-diary.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+DIARY_FILE="diary.txt"
+
+function main {
+  echo "Dear diary... Enter 'end' if you have no more entries for today."
+  echo "Who did you pair with?"
+  printf "> "
+  read PAIR_PARTNER
+  echo "What did you do?"
+  printf "> "
+  read DETAILS
+  echo "$(date '+%A %d %B %Y'), Paired with $(echo $PAIR_PARTNER | awk '{print toupper($0)}'): $DETAILS" >> $DIARY_FILE
+}
+
+main

ed07e92f678b320178263dc32646ea900cbf1889
Tom Barnes <tom.barnes@springer.com>
2017-04-26 18:36:38 +0100

    add Ruby script which actually works
    shame to give up on the bash version, but I can revisit it another time...

--- lib/top_ten.rb
+++ lib/top_ten.rb
@@ -1,9 +1,23 @@
-class TopTen
-    def top_ten(diary_entry, stopwords)
-        if diary_entry != stopwords
-            diary_entry
-	else
-	    ''
-	end
+#!/usr/bin/ruby
+
+require './lib/searcher'
+require 'optparse'
+
+options = {:diary_entries => [], :stopwords => []}
+
+parser = OptionParser.new do |opts|
+    opts.on('-d s', 'diary entries') do |diary|
+      diary.split.each do |word|
+        options[:diary_entries] << word
+      end
+    end
+    opts.on('-s s', 'stopwords') do |stopwords|
+      stopwords.split.each do |word|
+        options[:stopwords] << word
+      end
     end
 end
+
+parser.parse!
+searcher = Searcher.new(options[:stopwords])
+print "#{searcher.top_ten(options[:diary_entries])}\n"

a3e4dff73ddc3a9517880b40e32332e001db4977
Tom Barnes <tom.barnes@springer.com>
2017-06-23 15:39:42 +0100

    better full suite script and update README

--- full_suite.sh
+++ full_suite.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+GREEN=`tput setaf 6`
+NC=`tput sgr0`
+
+echo ""
+
+echo "${GREEN}Running Bash bats tests.....${NC}"
+echo ""
 ./test/top_ten_test.sh
+
+echo ""
+echo ""
+
+echo "${GREEN}Running Ruby rspec tests.....${NC}"
+echo ""
 rspec test
